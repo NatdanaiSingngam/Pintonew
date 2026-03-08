6e405e625529303510f034ca015d366fa7332b82
Natdanai Singngam <67020904@up.ac.th>
2026-03-08 17:38:23 +0700

    เปลี่ยนระบบจด

--- src/App.jsx
+++ src/App.jsx
@@ -189,11 +189,11 @@ function App() {
                   </button>
                 ) : (
                   <img 
-  src={currentUser?.photoURL || "https://www.svgrepo.com/show/529259/user-circle.svg"} 
-  alt="P" 
-  onClick={logout} 
-  className="w-10 h-10 rounded-full border-2 border-orange-200 cursor-pointer object-cover" 
-/>
+                      src={currentUser?.photoURL || "https://www.svgrepo.com/show/529259/user-circle.svg"} 
+                      alt="P" 
+                      onClick={logout} 
+                      className="w-10 h-10 rounded-full border-2 border-orange-200 cursor-pointer object-cover" 
+                    />
                 )}
               </div>
             </div>
